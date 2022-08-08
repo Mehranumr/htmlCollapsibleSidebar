48f5ead483ecabc6c91dea5d36f25478a0cc0040
Mehran Khan <mehranumr@gmail.com>
2022-08-08 23:27:21 +0500

    some new

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // the hello world program
 alert('Hello World');
 console.log('Hello World');
+
+var tag = document.createElement("p");
+var text = document.createTextNode("I am exprimenting something");
+tag.appendChild(text);
+var element = document.getElementById("new");
+element.appendChild(tag);
