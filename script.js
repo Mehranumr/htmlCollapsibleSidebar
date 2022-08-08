901d48c162b38b06193ce2f1c709aeba5483e20f
Mehran Khan <mehranumr@gmail.com>
2022-08-08 23:30:25 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// the hello world program
-alert('Hello World');
-console.log('Hello World');
-
-var tag = document.createElement("p");
-var text = document.createTextNode("I am exprimenting something");
-tag.appendChild(text);
-var element = document.getElementById("new");
-element.appendChild(tag);
